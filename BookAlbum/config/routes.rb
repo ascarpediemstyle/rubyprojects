929178f9f84cfdbc3837e314dbf166e728444362
ascarpediemstyle <aaa>
2015-07-11 05:56:22 -0700

    もろもろ追加

--- BookAlbum/config/routes.rb
+++ BookAlbum/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'infos/about'
+
   resources :books do 
     collection do
       get 'stat'
     end
   end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- BookAlbum/db/seeds.rb
+++ BookAlbum/db/seeds.rb
@@ -9,5 +9,11 @@
 publishers = Publisher.create(:name => 'ダイヤモンド社', :order_by => 1)
 publishers = Publisher.create(:name => '英治出版', :order_by => 2)
 
-categories = Category.create(:name => 'ビジネス',:order_by=>1)
+
 categories = Category.create(:name => '資格',:order_by=>1)
+categories = Category.create(:name => '組織',:order_by=>1)
+categories = Category.create(:name => 'リーダシップ',:order_by=>1)
+categories = Category.create(:name => '経済',:order_by=>1)
+categories = Category.create(:name => '市場',:order_by=>1)
+categories = Category.create(:name => 'マーケティング',:order_by=>1)
+categories = Category.create(:name => '人物/会社',:order_by=>1)
